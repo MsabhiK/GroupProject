ae48688300a8fc9ff84ade24bc15e8652560023c
saifkhmis <158212353+saifkhmis@users.noreply.github.com>
2025-05-18 21:34:08 +0100

    Delete taxi directory

--- taxi/src/main/java/com/taxi/controllers/TaxiController.java
+++
@@ -1,9 +0,0 @@
-package com.taxi.controllers;
-
-public class TaxiController {
-
-	public TaxiController() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- taxi/src/main/java/com/taxi/services/NotificationService.java
+++
@@ -1,9 +0,0 @@
-package com.taxi.services;
-
-public abstract class NotificationService {
-
-	public NotificationService() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
