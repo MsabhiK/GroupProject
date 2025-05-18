ed55075738d8db24418e8195653429707c364bc7
saifkhmis <158212353+saifkhmis@users.noreply.github.com>
2025-05-18 21:18:24 +0100

    Add files via upload

---
+++ taxi/src/main/java/com/taxi/controllers/TaxiController.java
@@ -0,0 +1,9 @@
+package com.taxi.controllers;
+
+public class TaxiController {
+
+	public TaxiController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ taxi/src/main/java/com/taxi/services/NotificationService.java
@@ -0,0 +1,9 @@
+package com.taxi.services;
+
+public abstract class NotificationService {
+
+	public NotificationService() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
